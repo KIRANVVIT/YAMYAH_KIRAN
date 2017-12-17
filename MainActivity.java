208a61b156a92bef105b7b4591b3209e9593ceb6
KIRANVVIT <addankiharikiran04@gmail.com>
2017-12-17 08:41:47 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,60 +1,66 @@
-package com.example.vihari.loginregister;
+package com.example.vihari.mike;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class  MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button bLogin;
-    EditText etUserName,etpassword;
-    TextView tvRegisterLink;
+import java.util.ArrayList;
+import java.util.Locale;
 
+import static android.R.attr.prompt;
 
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTEXT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        etUserName=(EditText) findViewById(R.id.etUsername);
-        etpassword=(EditText) findViewById(R.id.etPassword);
-        bLogin = (Button) findViewById(R.id.bLogin);
-        tvRegisterLink = (TextView) findViewById(R.id.tvRegisterLink);
-        bLogin.setOnClickListener(this);
-        tvRegisterLink.setOnClickListener(this);
-
-
+        resultTEXT = (TextView)findViewById(R.id.TVresult);
     }
-    @Override
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.bLogin:
-                if (etUserName.getText().toString().length() == 0)
-                    etUserName.setError("Enter Name");
-              else  if (etpassword.getText().toString().length() == 0)
-                    etpassword.setError("Enter Number");
-                else {
-                    startActivity(new Intent(this, Maps.class));
 
-                }
-                break;
-            case R.id.tvRegisterLink:
-                startActivity(new Intent(this,Register.class ));
-                break;
+    public void onButtonClick(View v)
+    {
+        if(v.getId() == R.id.imageButton)
+        {
+
+            promptSpeechInput();
         }
     }
 
-public void userReg(View view)
-{
-
-}
-    public  void userLogin(View view)
+    public void promptSpeechInput()
     {
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE , Locale.getDefault());
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT , "Say something to mike");
 
+        try {
+            startActivityForResult(i, 100);
+        }
+        catch (ActivityNotFoundException a)
+        {
+            Toast.makeText(MainActivity.this, "Sorry! device is not supporting" , Toast.LENGTH_LONG).show();
+        }
     }
 
+    public void onActivityResult(int request_code, int result_code, Intent i)
+    {
+        super.onActivityResult(request_code, result_code, i);
 
+            switch (request_code)
+            {
+                case 100: if (result_code == RESULT_OK && i != null)
+                {
+                    ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    resultTEXT.setText(result.get(0));
+                }
+                    break;
+            }
+    }
 }
